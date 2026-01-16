d2b005baca992f0a282e992a6864e1989a3b45c9
rubendrababu@gmail.com <rubendrababu@gmail.com>
2026-01-16 14:32:52 +0000

    added new files

---
+++ models/ruby_demo/customers.sql
@@ -0,0 +1,11 @@
+{{ config(
+    materialized = 'table'
+) }}
+
+select
+    customer_id,
+    first_name,
+    last_name,
+    email,
+    created_date
+from `bigquery-public-data.thelook_ecommerce.customers`
